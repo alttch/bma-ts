9e3a0459a560d4e6977b7ca86edac7dbba01e80d
Serhij S <s@g5.is>
2025-12-03 01:00:38 +0100

    0.2.4

--- src/impl_bincode.rs
+++ src/impl_bincode.rs
@@ -69,6 +69,9 @@ impl<C> Decode<C> for Monotonic {
     }
 }
 
+bincode::impl_borrow_decode!(Timestamp);
+bincode::impl_borrow_decode!(Monotonic);
+
 #[cfg(test)]
 mod tests {
     use super::{Monotonic, Timestamp};
